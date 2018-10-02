09733a6bc131eef85a9d7c8dd95e1ec83387c5e4
Zinaida <zinok.ilanka@yandex.ru>
2018-10-02 23:09:02 +0300

    project-setup

--- www/modules/main/index.php
+++ www/modules/main/index.php
@@ -13,7 +13,7 @@ ob_start();
 include ROOT. "templates/main/main.tpl";
 $content = ob_get_contents();
 ob_end_clean();
-
+ 
 include ROOT. "templates/_parts/_header.tpl";
 include ROOT. "templates/template.tpl";
 include ROOT. "templates/_parts/_footer.tpl";
--- www/templates/template.tpl
+++ www/templates/template.tpl
@@ -6,4 +6,4 @@
 
 <?php
 echo $content;
-?>
+?> 
